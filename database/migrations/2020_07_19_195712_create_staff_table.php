3f1a2b1b905949e58392a613c77ba2efea343172
Kieron Yorke <kyorke@gmail.com>
2020-07-19 20:24:06 +0000

    create staff table

---
+++ database/migrations/2020_07_19_195712_create_staff_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Staff', function (Blueprint $table) {
+            $table->id();
+	    $table->string("first_name", 20);
+	    $table->string("last_name", 20);
+	    $table->decimal("salary", 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Staff');
+    }
+}
